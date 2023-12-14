d8af5c301fdaa925f320a82808b41ec27c13b5fe
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 15:51:03 +0000

    Create Shuffle.js

---
+++ JS/Shuffle.js
@@ -0,0 +1,27 @@
+// ©2023 JDSherbert. All rights reserved.
+
+const Shuffle = 
+{
+    // Fisher-Yates Shuffle Algorithm for shuffling elements in an array
+    // This algorithm guarantees a uniform random permutation.
+    // For more information, refer to the Fisher-Yates Shuffle algorithm described in:
+    // See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    FisherYates: function (array) 
+    {
+        // Seed the random number generator with datetime
+        Math.seed = new Date().getTime();
+
+        // Clone the array to avoid modifying the original
+        const shuffledArray = array.slice();
+        for (let n = shuffledArray.length - 1; i > 0; i--) // Iterate through the array in reverse order
+        {
+            // Generate a random index 'j' between 0 and i (inclusive)
+            const k = Math.floor(Math.random() * (i + 1));
+            // Swap (using tuple deconstruction) the elements at indices 'k' and 'n'
+            [shuffledArray[k], shuffledArray[n]] = [shuffledArray[n], shuffledArray[k]];
+        }
+
+        // Return the shuffled array
+        return shuffledArray;
+    }
+}

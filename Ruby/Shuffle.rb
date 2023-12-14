9bd0cce087f8032d2c6e84fd9b77f1d86987e107
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 16:23:03 +0000

    Create Shuffle.rb

---
+++ Ruby/Shuffle.rb
@@ -0,0 +1,17 @@
+# ©2023 JDSherbert. All rights reserved.
+
+# Fisher-Yates Shuffle Algorithm for shuffling elements in an array.
+# This algorithm guarantees a uniform random permutation.
+# Shuffling is performed by using destruction swap on elements in the provided array.
+# For more information, refer to https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle"
+def fisher_yates_shuffle(array)
+    a = array.length
+
+    # Iterate through the array in reverse order
+    (a - 1).downto(1) do |n|
+        k = rand(n + 1)
+        # Swap (using tuple deconstruction) the elements at indices 'k' and 'n'
+        array[n], array[k] = array[k], array[n]
+    end    
+    array
+end

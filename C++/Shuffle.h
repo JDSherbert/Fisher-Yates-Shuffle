e62afcdf6959d1ddfc2f7374fcc5bc6acdfbdbe1
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 03:33:46 +0000

    Update Shuffle.h

--- C++/Shuffle.h
+++ C++/Shuffle.h
@@ -1,1 +1,16 @@
+// ©2023 JDSherbert. All rights reserved.
 
+#pragma once
+
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+class Shuffle 
+{
+public:
+
+    template <typename T, size_t N>
+    static void FisherYates(T (&array)[N]);
+
+};

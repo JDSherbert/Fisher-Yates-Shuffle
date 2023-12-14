9f23495ddd4ced05b6b6b63bb58ec275b9e11b49
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 03:37:40 +0000

    Update Shuffle.h

--- C++/Shuffle.h
+++ C++/Shuffle.h
@@ -6,10 +6,21 @@
 #include <ctime>
 #include <algorithm>
 
+/// <summary>
+/// Utility class for shuffling arrays using various algorithms.
+/// </summary>
 class Shuffle 
 {
 public:
 
+    /// <summary>
+    /// Fisher-Yates Shuffle Algorithm for shuffling elements in an array.
+    /// This algorithm guarantees a uniform random permutation.
+    /// For more information, refer to the Fisher-Yates Shuffle algorithm described in:
+    /// See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the array.</typeparam>
+    /// <param name="array">The array to shuffle.</param>
     template <typename T, size_t N>
     static void FisherYates(T (&array)[N]);
 

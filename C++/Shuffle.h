19dfdf199eec50e649d49ab6a0a6c8d08f2b049d
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 04:28:49 +0000

    Update Shuffle.h

--- C++/Shuffle.h
+++ C++/Shuffle.h
@@ -6,22 +6,41 @@
 #include <ctime>
 #include <algorithm>
 
-/// <summary>
-/// Utility class for shuffling arrays using various algorithms.
-/// </summary>
+/**
+ * Utility class for shuffling arrays using various algorithms.
+ */
 class Shuffle 
 {
 public:
 
-    /// <summary>
-    /// Fisher-Yates Shuffle Algorithm for shuffling elements in an array.
-    /// This algorithm guarantees a uniform random permutation.
-    /// For more information, refer to the Fisher-Yates Shuffle algorithm described in:
-    /// See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the array.</typeparam>
-    /// <param name="array">The array to shuffle.</param>
+    /**
+     * Fisher-Yates Shuffle Algorithm for shuffling elements in an array.
+     * This algorithm guarantees a uniform random permutation.
+     * For more information, refer to the Fisher-Yates Shuffle algorithm described in:
+     * See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+     *
+     * @tparam T The type of elements in the array.
+     * @tparam N The size of the array.
+     * @param array The array to shuffle.
+     * @returns the original array, shuffled.
+     */
     template <typename T, size_t N>
     static void FisherYates(T (&array)[N]);
+
+    /**
+     * Fisher-Yates Shuffle Algorithm for shuffling elements in an array.
+     * This algorithm guarantees a uniform random permutation.
+     * For more information, refer to the Fisher-Yates Shuffle algorithm described in:
+     * See https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+     *
+     * @tparam T The type of elements in the array.
+     * @tparam N The size of the array.
+     * @param array The array to shuffle.
+     * @returns a copy of the original array, shuffled.
+     */
+    template <typename T>
+    static T FisherYates(const T& inputArray);
+
+}
 
 };

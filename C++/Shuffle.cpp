76a52cad44328b258b287c3c28e273ee0192bf59
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-14 03:36:35 +0000

    Update Shuffle.cpp

--- C++/Shuffle.cpp
+++ C++/Shuffle.cpp
@@ -1,1 +1,18 @@
+// ©2023 JDSherbert. All rights reserved.
 
+#include "Shuffle.h"
+
+template <typename T, size_t N>
+void Shuffle::FisherYates(T (&array)[N]) 
+{
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    size_t n = N;
+    while (n > 1) 
+    {
+        n--;
+        
+        size_t k = std::rand() % (n + 1);
+        std::swap(array[k], array[n]);
+    }
+}
